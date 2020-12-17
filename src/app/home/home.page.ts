db5d6f96adc648822a1984bb0d701923bd00d04b
Jonathan Perry <jpper@umich.edu>
2020-12-17 17:44:29 +0900

    Finished up how to build your own ionic library for npm driver app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,18 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { JonnyramenLibService } from "jonnyramen-lib";
 
 @Component({
-  selector: 'app-home',
-  templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  selector: "app-home",
+  templateUrl: "home.page.html",
+  styleUrls: ["home.page.scss"],
 })
 export class HomePage {
+  user = null;
+  constructor(private jonnyramenLibService: JonnyramenLibService) {}
 
-  constructor() {}
-
+  getData() {
+    this.jonnyramenLibService.getData().subscribe((res) => {
+      this.user = res;
+    });
+  }
 }
